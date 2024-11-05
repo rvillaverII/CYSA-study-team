d7830ede9a49992520b96760836d6660b0a976cb
RetroCoding1 <nickandretrocoding@gmail.com>
2024-11-05 20:09:36 +0000

    Bash Script Version 2 complete

--- getip.sh
+++ getip.sh
@@ -2,7 +2,7 @@
 # This script displays the IP address of the machine
 
 # Get the IP address using the hostname command
-ip_address=$(ifconfig)
+ip_address=$(hostname -I | awk '{print $1}')
 
 # Display the IP address
 echo "The IP address of this machine is: $ip_address"
