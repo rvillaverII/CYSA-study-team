62a9967bac4af778cda2a86d5e20a812d067ab25
KaitlynKramp <kaitlynkramp7@gmail.com>
2024-11-05 19:33:13 +0000

    Created IPScript.sh to show ip addresses

---
+++ student1/log-analysis/IPScript.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/bash
+ipv4=$(ifconfig ens32 | grep -oP 'inet \K[^ ]+')
+ipv6=$(ifconfig ens32 | grep -oP 'inet6 \K[^ ]+')
+interface=ens32
+
+echo "Your IPv4 address is "$ipv4
+echo "Your IPv6 address is "$ipv6
